8e10b395577d535b819fbb74bf48efa7e5aac0ae
himanshu007-creator <65963997+himanshu007-creator@users.noreply.github.com>
2020-10-05 19:00:35 +0530

    updated.cpp

--- Code.cpp
+++ Code.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class student
 {
 	char name[20];
-	/*p,c,m are subjects ohysics,chemistrty ,maths.*/
+	/p,c,m are subjects ohysics,chemistrty ,maths./
 	float p,c,m,t;
 	public:
 	void get(char a[20],int y,int x,int z)
@@ -21,65 +21,65 @@ class student
 	{
 		if(a.t>b.t)
 		{
-			cout<<a.name <<" scored more marks than "<<b.name<<endl;
+			cout<<a.name <<" SCORED MORE THAN "<<b.name<<endl;
 		}
 		else if(a.t<b.t)
 		{
-			cout<<b.name <<" scored more marks than "<<a.name<<endl;
+			cout<<b.name <<" SCORED MORE THAN "<<a.name<<endl;
 		}
 		else
 		{
-			cout<<a.name <<" and "<<b.name<<" scored equal!"<<endl;
+			cout<<a.name <<" AND "<<b.name<<" SCORED EQUAL!"<<endl;
 		}
 	}
 	void c_physics(student a, student b)
 	{	if(a.p>b.p)
 		{
-			cout<<a.name <<" scored more marks in physics than "<<b.name<<endl;
+			cout<<a.name <<" SCORED MORE MARKS IN PHYSICS THAN  "<<b.name<<endl;
 		}
 		else if(a.p<b.p)
 		{
-			cout<<b.name <<" scored more marks in physics than "<<a.name<<endl;
+			cout<<b.name <<" SCORED MORE MARKS IN PHYSICS THAN "<<a.name<<endl;
 		}
 		else
 		{
-			cout<<a.name <<" and "<<b.name<<" scored equal in physics!"<<endl;
+			cout<<a.name <<" AND "<<b.name<<" SCORED EQUAL IN PHYSICS!"<<endl;
 		}
 	}
 	void c_maths(student a, student b)
 	{if(a.m>b.m)
 		{
-			cout<<a.name <<" scored more marks in maths than "<<b.name<<endl;
+			cout<<a.name <<" SCORED MORE MARKS IN MATHS THAN "<<b.name<<endl;
 		}
 		else if(a.m<b.m)
 		{
-			cout<<b.name <<" scored more marks in maths than "<<a.name<<endl;
+			cout<<b.name <<" SCORED MORE MARKS IN MATHS THAN "<<a.name<<endl;
 		}
 		else
 		{
-			cout<<a.name <<" and "<<b.name<<" scored equal in maths!"<<endl;
+			cout<<a.name <<" AND "<<b.name<<" SCORED EQUAL IN MATHS!"<<endl;
 		}
 	}
 	void c_chemistry(student a,student b)
 	{if(a.c>b.c)
 		{
-			cout<<a.name <<" scored more marks in chemistry than "<<b.name<<endl;
+			cout<<a.name <<" SCORED MORE MARKS IN CHEMISTRY THAN "<<b.name<<endl;
 		}
 		else if(a.m<b.m)
 		{
-			cout<<b.name <<" scored more marks in maths than "<<a.name<<endl;
+			cout<<b.name <<" SCORED MORE MARKS IN CHEMISTRY THAN "<<a.name<<endl;
 		}
 		else
 		{
-			cout<<a.name <<" and "<<b.name<<" scored equal in maths!"<<endl;
+			cout<<a.name <<" AND "<<b.name<<" SCORED EQUAL IN MATHS!"<<endl;
 		}
 	}
 };
 int main()
 {
 	student s1,s2,s3;
-	s1.get('GARRY',50,60,70);
-	s2.get('BRAHMAJEET',49,70,69);
+	s1.get("GARRY",50,60,70);
+	s2.get("BRAHMAJEET",49,70,69);
 	s3.c_physics(s1,s2);
 	s3.c_chemistry(s1,s2);
 	s3.c_maths(s1,s2);
